8f814ffec83c2ec7106c70100a05e55baad6e5f9
Ngô Văn Tiến Đạt <dat.ngovantien@di4l.vn>
2024-09-14 13:38:10 +0700

    Update base login, register controller

--- lib/views/login_screen/getx/login.controller.dart
+++ lib/views/login_screen/getx/login.controller.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:freal_flutter/core/base/base.api.dart';
 import 'package:freal_flutter/core/base/base.controller.dart';
+import 'package:freal_flutter/models/api.response.dart';
 import 'package:freal_flutter/requests/auth.request.dart';
 import 'package:localize_and_translate/localize_and_translate.dart';
 
@@ -10,16 +10,22 @@ class LoginController extends BaseController {
   final TextEditingController _username = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
+  // Handle login
   Future<void> handleLogin() async {
     if (_username.text.isEmpty || _password.text.isEmpty) {
       showMessage("Please fill username and password".tr());
     } else {
       showLoading();
       // Handle request
-      final response = _authRequest.loginRequest(
+      final response = await _authRequest.loginRequest(
         email: _username.text,
         password: _password.text,
       );
+
+      await handleDeviceLogin(response);
     }
+    hideLoading();
   }
+
+  Future<void> handleDeviceLogin(ApiResponse response) async {}
 }
--- lib/views/register_screen/getx/register.controller.dart
+++ lib/views/register_screen/getx/register.controller.dart
@@ -1,3 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:freal_flutter/core/base/base.controller.dart';
+import 'package:freal_flutter/models/api.response.dart';
+import 'package:freal_flutter/requests/auth.request.dart';
+import 'package:localize_and_translate/localize_and_translate.dart';
 
-class RegisterController extends BaseController {}
+class RegisterController extends BaseController {
+  final AuthRequest _authRequest = AuthRequest();
+
+  final TextEditingController _fullname = TextEditingController();
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  Future<void> handleRegister() async {
+    if (_fullname.text.isEmpty ||
+        _username.text.isEmpty ||
+        _password.text.isEmpty) {
+      showMessage("Please fill username and password".tr());
+    } else {
+      showLoading();
+      final response = await _authRequest.register(
+        fullname: _fullname.text,
+        email: _username.text,
+        password: _password.text,
+      );
+
+      await handleDeviceRegister(response);
+    }
+
+    hideLoading();
+  }
+
+  Future<void> handleDeviceRegister(ApiResponse response) async {}
+}
