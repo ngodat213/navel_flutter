17672f5bd9ac5dd9ed64667c0e651837fa2d5db8
Ngô Văn Tiến Đạt <dat.ngovantien@di4l.vn>
2024-09-14 13:27:27 +0700

    Config firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:freal_flutter/core/constants/constants.dart';
 import 'package:freal_flutter/my_app.dart';
 import 'package:localize_and_translate/localize_and_translate.dart';
+import 'firebase_options.dart';
 
 void main() async {
   await runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
       //setting up firebase notifications
-      await Firebase.initializeApp();
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
 
       await LocalizeAndTranslate.init(
         defaultType: LocalizationDefaultType.asDefined,
